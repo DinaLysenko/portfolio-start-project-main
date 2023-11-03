a15c29db6bfddbf70c73af66b6910d52e5fb9656
DinaLysenko <dinafate@rambler.ru>
2023-11-03 13:24:56 +0500

    add @media for main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,12 +4,13 @@ import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={'center'} justify={'space-between'}>
+                <FlexWrapper align={'center'} justify={'space-around'} wrap={'wrap'}>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Dina Lysenko</span></Name>
@@ -28,10 +29,16 @@ const StyledMain = styled.section`
   min-height: 100vh;
   display: flex;
 
+  @media ${theme.media.tablet} {
+    justify-content: center;
+    align-items: center;
+  }
 `
 const PhotoWrapper=styled.div`
   position: relative;
   z-index: 0;
+  margin-right: 20px;
+  margin-top: 65px;
   
   &::before {
     content: '';
@@ -42,6 +49,11 @@ const PhotoWrapper=styled.div`
     top: -24px;
     left: 24px;
     z-index: -1;
+    
+    @media ${theme.media.mobile} {
+      width: 314px;
+      height: 414px;
+    }
   }
 `
 
@@ -49,25 +61,32 @@ const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
- 
- 
+  
+  @media ${theme.media.mobile} {
+    width: 310px;
+    height: 380px;
+  }
+
 `
 
 const SmallText = styled.span`
   font-size: 14px;
   font-weight: 400;
-
+ 
 `
 const Name = styled.h2`
-  font-family: Josefin Sans, sans-serif;
-  font-size: 50px;
-  font-weight: 700;
+  ${font({family: 'Josefin Sans, sans-serif', Fmax: 50, Fmin: 36, weight: 700})}
   letter-spacing: 0.05em;
   margin: 10px 0;
+  
+  @media ${theme.media.tablet} {
+    margin: 15px 0 22px ;
+  }
 
   span {
     position: relative;
     z-index: 0;
+    white-space: nowrap;
 
     &::before {
       content: '';
@@ -83,7 +102,5 @@ const Name = styled.h2`
   }
 `
 const MainTitle = styled.h1`
-  font-size: 27px;
-  font-weight: 400;
-
+  ${font({Fmax: 27, Fmin: 20, weight: 400})}
 `
