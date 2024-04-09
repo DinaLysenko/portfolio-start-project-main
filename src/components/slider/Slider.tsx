f3fc26a7de7586287d639f85748a50bffe210ecf
DinaLysenko <dinafate@rambler.ru>
2024-04-09 23:15:55 +0500

    add AliceCarousel library

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,29 +1,25 @@
 import React from 'react';
-import styled from "styled-components";
-import {FlexWrapper} from "../FlexWrapper";
-import {Icon} from "../icon/Icon";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import styled from 'styled-components';
 
 
-export const Slider = () => {
-    return (
-        <StyledSlider>
-            <FlexWrapper>
-                <Slide>
-                    <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit.</Text>
-                    <Name>@ivan ivanow</Name>
-                </Slide>
-            </FlexWrapper>
-            <Icon iconId={'pagination'}/>
-            {/*<Pagination>*/}
-            {/*    <span> </span>*/}
-            {/*    <span> </span>*/}
-            {/*    <span> </span>*/}
-            {/*</Pagination>*/}
-        </StyledSlider>
-    );
-};
+const items = [
+    <div className="item" data-value="1">1</div>,
+    <div className="item" data-value="2">2</div>,
+    <div className="item" data-value="3">3</div>,
+    <div className="item" data-value="4">4</div>,
+    <div className="item" data-value="5">5</div>,
+];
+
+export const Slider = () => (
+    <StyledSlider>
+    <AliceCarousel
+        mouseTracking
+        items={items}
+    />
+    </StyledSlider>
+);
 
 const StyledSlider = styled.div`
   max-width: 500px;
@@ -32,21 +28,48 @@ const StyledSlider = styled.div`
   align-items: center;
 `
 
-const Slide = styled.div`
-  text-align: center;
-`
-const Text = styled.p`
+// const Slide = styled.div`
+//   text-align: center;
+// `
+// const Text = styled.p`
+//
+// `
+// const Name = styled.span`
+//   display: inline-block;
+//   margin: 22px 0 42px;
+//   font-family: Josefin San, sans-serif;
+//   font-size: 16px;
+//   font-weight: 600;
+//   letter-spacing: 1px;
+//   text-transform: uppercase;
+// `
 
-`
-const Name = styled.span`
-  display: inline-block;
-  margin: 22px 0 42px;
-  font-family: Josefin San, sans-serif;
-  font-size: 16px;
-  font-weight: 600;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-`
+// import React from 'react';
+// import styled from "styled-components";
+// import {FlexWrapper} from "../FlexWrapper";
+// import {Icon} from "../icon/Icon";
+//
+//
+// export const Slider = () => {
+//     return (
+//         <StyledSlider>
+//             <FlexWrapper>
+//                 <Slide>
+//                     <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+//                         labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
+//                         elit.</Text>
+//                     <Name>@ivan ivanow</Name>
+//                 </Slide>
+//             </FlexWrapper>
+//             <Icon iconId={'pagination'}/>
+//             {/*<Pagination>*/}
+//             {/*    <span> </span>*/}
+//             {/*    <span> </span>*/}
+//             {/*    <span> </span>*/}
+//             {/*</Pagination>*/}
+//         </StyledSlider>
+//     );
+// };
 // const Pagination = styled.div`
   //span {
   //  display: inline-block;
