41d13fcd3694e21043a590faea7bbce806a32ff0
DinaLysenko <dinafate@rambler.ru>
2024-06-07 23:06:15 +0500

    AliceCarousel

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -3,79 +3,71 @@ import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import styled from 'styled-components';
 
-
+type SlideProps = {
+    text: string
+    userName: string
+}
+export const Slide = ({text, userName}: SlideProps) => {
+    return (
+        <SlideStyle>
+            <Text>{text}</Text>
+            <Name>@{userName}</Name>
+        </SlideStyle>
+    )
+}
 const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
+    <Slide userName='petr petrov' text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit."/>,
+    <Slide userName='ivan ivanovv' text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit."/>,
+    <Slide userName='alex sidorov' text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit."/>
 ];
 
 export const Slider = () => (
     <StyledSlider>
-    <AliceCarousel
-        mouseTracking
-        items={items}
-    />
+        <AliceCarousel
+            mouseTracking
+            items={items}
+        />
     </StyledSlider>
 );
 
 const StyledSlider = styled.div`
-  max-width: 500px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    max-width: 500px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
-// const Slide = styled.div`
-//   text-align: center;
-// `
-// const Text = styled.p`
-//
-// `
-// const Name = styled.span`
-//   display: inline-block;
-//   margin: 22px 0 42px;
-//   font-family: Josefin San, sans-serif;
-//   font-size: 16px;
-//   font-weight: 600;
-//   letter-spacing: 1px;
-//   text-transform: uppercase;
-// `
+const SlideStyle = styled.div`
+    text-align: center;
+`
+const Text = styled.p`
 
-// import React from 'react';
-// import styled from "styled-components";
-// import {FlexWrapper} from "../FlexWrapper";
-// import {Icon} from "../icon/Icon";
-//
-//
-// export const Slider = () => {
-//     return (
-//         <StyledSlider>
-//             <FlexWrapper>
-//                 <Slide>
-//                     <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-//                         labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
-//                         elit.</Text>
-//                     <Name>@ivan ivanow</Name>
-//                 </Slide>
-//             </FlexWrapper>
-//             <Icon iconId={'pagination'}/>
-//             {/*<Pagination>*/}
-//             {/*    <span> </span>*/}
-//             {/*    <span> </span>*/}
-//             {/*    <span> </span>*/}
-//             {/*</Pagination>*/}
-//         </StyledSlider>
-//     );
-// };
+`
+
+const Name = styled.span`
+    display: inline-block;
+    margin: 22px 0 42px;
+    font-family: Josefin San, sans-serif;
+    font-size: 16px;
+    font-weight: 600;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+`
+
+
 // const Pagination = styled.div`
-  //span {
-  //  display: inline-block;
-  //  height: 10px;
-  //  width: 10px;
-  //  background-color: #701c9a;
-  //  margin: 5px;
-  //}
+//span {
+//  display: inline-block;
+//  height: 10px;
+//  width: 10px;
+//  background-color: #701c9a;
+//  margin: 5px;
+//}
 // `
